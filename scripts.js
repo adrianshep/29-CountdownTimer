7f8659533cfcca0ecc9ba5335005bdf904386bc8
Adrian Shepelavey <adrianshepelavey@gmail.com>
2018-07-26 13:10:26 -0400

    created timer and displayTimeLeft functions

--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
 let countdown;
+
+function timer(seconds) {
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    // check if we should stop timer
+    if (secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+    // display timer
+    displayTimeLeft(secondsLeft);
+  }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+
+}
