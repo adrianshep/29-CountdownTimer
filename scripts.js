857e4ea24ab6b68992c21ebd40021bd9eb926c57
Adrian Shepelavey <adrianshepelavey@gmail.com>
2018-07-27 12:50:03 -0400

    created displayEndTime function

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
 let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
 
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
+  displayEndTime(then);
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -18,5 +20,16 @@ function timer(seconds) {
 }
 
 function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
+  document.title = display;
+  timerDisplay.textContent = display;
+}
 
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be Back At ${hour}:${minutes}`;
 }
